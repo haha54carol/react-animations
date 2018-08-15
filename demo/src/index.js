682b8a7662b900a4fa990eed80f79ffb8089df1d
Carol Ni <carol.ni@sapphireinfo.com.tw>
2018-08-15 18:24:28 +0800

    init

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,14 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
 import Example from '../../src'
 
 class Demo extends Component {
   render() {
     return <div>
-      <h1>react-animations Demo</h1>
-      <Example/>
+      <Example />
     </div>
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
---
+++ src/animation.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { CSSTransition } from 'react-transition-group'
+
+const propTypes = {
+    in: PropTypes.bool,
+    act: PropTypes.string.isRequired
+};
+
+const Animation = ({ children, ...props }) => {
+    const { act, ...rest } = props
+    const animateType = act ? act : ''
+
+    const mergeProps = Object.assign({
+        classNames: animateType,
+        key: animateType,
+        timeout: { enter: 300, exit: 200 },
+        in: true,
+        mountOnEnter: true,
+        unmountOnExit: true,
+    }, rest)
+
+    return (
+        <CSSTransition {...mergeProps}>
+            {children}
+        </CSSTransition>
+    )
+}
+
+
+Animation.propTypes = propTypes;
+
+export default Animation
