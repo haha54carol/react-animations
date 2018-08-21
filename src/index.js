a75a3d2bb8a382e0d823e557c9b67f1b78beec6e
Carol Ni <carol.ni@sapphireinfo.com.tw>
2018-08-21 15:10:32 +0800

    Updates

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import Animation from '../../src/'
+import '../../src/css/app.css'
+
+class Demo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      animations: [
+        'topIn',
+        'bottomIn',
+        'leftIn',
+        'rightIn',
+        'topInBottomOut',
+        'zoomIn',
+        'fade'
+      ],
+      exit: false
+    }
+
+  }
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="title">React-Animations - Examples</div>
+        <div className="btn" onClick={() => this.setState({ exit: !this.state.exit })}>
+          Click Me
+        </div>
+        <a className="link" href="https://github.com/haha54carol/react-animations" onClick={() => this.setState({ exit: !this.state.exit })}>
+          Github⭐
+        </a>
+        <div className="container">
+          {
+            this.state.animations.map(t =>
+              <div className="col-4" key={t}>
+                <div className="typeName">{t}</div>
+                <div className="boxContainer">
+                  <Animation act={t} in={this.state.exit}>
+                    <div className="box"></div>
+                  </Animation>
+                </div>
+              </div>
+            )
+          }
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+render(<Demo />, document.querySelector('#demo'))
